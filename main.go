67cf26c94eecdccdb55bf395e9846d9b9cc866bc
bgpat <bgpat@users.noreply.github.com>
2018-05-13 21:32:04 +0900

    Return also NS and SOA records

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net"
-	"os"
 	"strings"
 
 	"github.com/miekg/dns"
@@ -14,45 +14,62 @@ import (
 var (
 	addr      string
 	port      string
-	protocols []string
-	domain    string
-
-	ttl     = uint32(60)
-	mbox    string
-	serial  uint32
-	refresh = uint32(3600)
-	retry   = uint32(900)
-	expire  = uint32(604800)
-	minttl  = uint32(3600)
+	protocols = stringList{"tcp", "udp"}
+	domains   = stringList{"."}
+	ttl       = uint64(60)
+	mbox      string
+	serial    uint64
+	refresh   = uint64(3600)
+	retry     = uint64(900)
+	expire    = uint64(604800)
+	minttl    = uint64(3600)
+	adminUser = "admin"
 )
 
+type stringList []string
+
+func (s *stringList) Set(value string) error {
+	if value == "" {
+		return nil
+	}
+	*s = strings.Split(value, ",")
+	return nil
+}
+
+func (s *stringList) String() string {
+	return strings.Join(*s, ",")
+}
+
+func init() {
+	flag.StringVar(&addr, "addr", "", "listen address")
+	flag.StringVar(&port, "port", "53", "listen port")
+	flag.Var(&protocols, "proto", "listen protocol list")
+	flag.Var(&domains, "domain", "domain list")
+	flag.Uint64Var(&ttl, "ttl", ttl, "TTL")
+	flag.StringVar(&mbox, "mbox", adminUser+"@<domain>", "SOA mbox")
+	flag.Uint64Var(&serial, "serial", serial, "SOA serial")
+	flag.Uint64Var(&refresh, "refresh", refresh, "SOA refresh")
+	flag.Uint64Var(&expire, "expire", expire, "SOA expire")
+	flag.Uint64Var(&minttl, "minttl", minttl, "SOA minttl")
+}
+
 func main() {
-	addr = os.Getenv("ADDR")
-
-	port = os.Getenv("PORT")
-	if port == "" {
-		port = "53"
+	flag.Parse()
+	if mbox == "" {
+		mbox = adminUser
 	}
-
-	proto := os.Getenv("PROTO")
-	if proto == "" {
-		protocols = []string{"tcp", "udp"}
-	} else {
-		protocols = strings.Split(proto, ",")
-	}
-
-	domain = dns.Fqdn(os.Getenv("DOMAIN"))
-	mbox = dns.Fqdn("admin." + domain)
 
 	g := errgroup.Group{}
 	for _, proto := range protocols {
-		server := &dns.Server{
-			Addr: addr + ":" + port,
-			Net:  proto,
+		for _, domain := range domains {
+			server := &dns.Server{
+				Addr: addr + ":" + port,
+				Net:  proto,
+			}
+			dns.HandleFunc(domain, handleRequest(domain))
+			g.Go(server.ListenAndServe)
+			log.Println("INFO\tlisten domain=%s", server.Net, server.Addr, domain)
 		}
-		dns.HandleFunc(domain, handleRequest)
-		g.Go(server.ListenAndServe)
-		log.Println("INFO\tlisten", server.Net, server.Addr)
 	}
 	fmt.Println("")
 	if err := g.Wait(); err != nil {
@@ -60,65 +77,71 @@ func main() {
 	}
 }
 
-func handleRequest(w dns.ResponseWriter, r *dns.Msg) {
-	m := new(dns.Msg)
-	m.SetReply(r)
-	for _, q := range r.Question {
-		log.Printf("INFO [%04x]\trequest: remote=%v qtype=%v name=%v\n", r.MsgHdr.Id, w.RemoteAddr(), dns.TypeToString[q.Qtype], q.Name)
-		if q.Qclass != dns.ClassINET {
-			log.Printf("ERROR[%04x]\tunsupported qclass: %s\n", r.MsgHdr.Id, dns.ClassToString[q.Qclass])
-			continue
-		}
-
-		switch q.Qtype {
-		case dns.TypeA:
-			req := net.ParseIP(strings.TrimSuffix(q.Name, "."+domain)).To4()
-			if req == nil || req.IsUnspecified() {
-				log.Printf("ERROR[%04x]\tinvalid domain name: %s\n", r.MsgHdr.Id, q.Name)
+func handleRequest(domain string) dns.HandlerFunc {
+	mbox := strings.Replace(mbox, "@", ".", -1)
+	if mbox == "" {
+		mbox = adminUser + "." + domain
+	}
+	return func(w dns.ResponseWriter, r *dns.Msg) {
+		m := new(dns.Msg)
+		m.SetReply(r)
+		for _, q := range r.Question {
+			log.Printf("INFO [%04x]\trequest: remote=%v qtype=%v name=%v\n", r.MsgHdr.Id, w.RemoteAddr(), dns.TypeToString[q.Qtype], q.Name)
+			if q.Qclass != dns.ClassINET {
+				log.Printf("ERROR[%04x]\tunsupported qclass: %s\n", r.MsgHdr.Id, dns.ClassToString[q.Qclass])
 				continue
 			}
 
-			m.Answer = append(m.Answer, &dns.A{
-				Hdr: dns.RR_Header{
-					Name:   q.Name,
-					Rrtype: dns.TypeA,
-					Class:  dns.ClassINET,
-					Ttl:    ttl,
-				},
-				A: req,
-			})
-		case dns.TypeNS:
-			m.Answer = append(m.Answer, &dns.NS{
-				Hdr: dns.RR_Header{
-					Name:   q.Name,
-					Rrtype: dns.TypeNS,
-					Class:  dns.ClassINET,
-					Ttl:    ttl,
-				},
-				Ns: domain,
-			})
-		case dns.TypeSOA:
-			m.Answer = append(m.Answer, &dns.SOA{
-				Hdr: dns.RR_Header{
-					Name:   q.Name,
-					Rrtype: dns.TypeSOA,
-					Class:  dns.ClassINET,
-					Ttl:    ttl,
-				},
-				Ns:      domain,
-				Mbox:    mbox,
-				Serial:  serial,
-				Refresh: refresh,
-				Retry:   retry,
-				Expire:  expire,
-				Minttl:  minttl,
-			})
-		default:
-			log.Printf("ERROR[%04x]\tunsupported qtype: %s\n", r.MsgHdr.Id, dns.TypeToString[q.Qtype])
-		}
+			switch q.Qtype {
+			case dns.TypeA:
+				req := net.ParseIP(strings.TrimSuffix(q.Name, "."+domain)).To4()
+				if req == nil || req.IsUnspecified() {
+					log.Printf("ERROR[%04x]\tinvalid domain name: %s\n", r.MsgHdr.Id, q.Name)
+					continue
+				}
 
-	}
-	if err := w.WriteMsg(m); err != nil {
-		log.Printf("ERROR[%04x]\t %v", r.MsgHdr.Id, err)
+				m.Answer = append(m.Answer, &dns.A{
+					Hdr: dns.RR_Header{
+						Name:   q.Name,
+						Rrtype: dns.TypeA,
+						Class:  dns.ClassINET,
+						Ttl:    uint32(ttl),
+					},
+					A: req,
+				})
+			case dns.TypeNS:
+				m.Answer = append(m.Answer, &dns.NS{
+					Hdr: dns.RR_Header{
+						Name:   q.Name,
+						Rrtype: dns.TypeNS,
+						Class:  dns.ClassINET,
+						Ttl:    uint32(ttl),
+					},
+					Ns: domain,
+				})
+			case dns.TypeSOA:
+				m.Answer = append(m.Answer, &dns.SOA{
+					Hdr: dns.RR_Header{
+						Name:   q.Name,
+						Rrtype: dns.TypeSOA,
+						Class:  dns.ClassINET,
+						Ttl:    uint32(ttl),
+					},
+					Ns:      domain,
+					Mbox:    mbox,
+					Serial:  uint32(serial),
+					Refresh: uint32(refresh),
+					Retry:   uint32(retry),
+					Expire:  uint32(expire),
+					Minttl:  uint32(minttl),
+				})
+			default:
+				log.Printf("ERROR[%04x]\tunsupported qtype: %s\n", r.MsgHdr.Id, dns.TypeToString[q.Qtype])
+			}
+
+		}
+		if err := w.WriteMsg(m); err != nil {
+			log.Printf("ERROR[%04x]\t %v", r.MsgHdr.Id, err)
+		}
 	}
 }
